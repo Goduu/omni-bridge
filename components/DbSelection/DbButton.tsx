aa75814b5c1c790b177847973dd8b7d9b0f74ebd
Goduu <idcangussu@hotmail.com>
2024-03-05 21:31:33 +0100

    Fix selected button bg

--- components/DbSelection/DbButton.tsx
+++ components/DbSelection/DbButton.tsx
@@ -8,10 +8,11 @@ type DbButtonProps = {
 }
 
 export const DbButton: FC<DbButtonProps> = ({ text, icon, active, onClick }) => {
+
     return (
         <button
             onClick={onClick}
-            className={`group flex flex-col bg-white/30 p-4 aspect-square justify-center shadow-xl ring-1 ring-gray-900/5 rounded-lg backdrop-blur-lg items-center cursor-pointer hover:shadow-lg active:shadow-sm ${active && "bg-slate-300"}`}
+            className={`group flex flex-col p-4 aspect-square justify-center shadow-xl ring-1 ring-gray-900/5 rounded-lg backdrop-blur-lg items-center cursor-pointer hover:shadow-lg active:shadow-sm ${active ? "bg-slate-300" : "bg-white/30"}`}
         >
             <div className='group-hover:scale-125 duration-150'
             >
